fc3f38351b137ba908be9ff86bfd46e00549b10d
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-02 11:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ function convertToRoman(num) {
     };
 
   //your code here
+	const map = new Map([
+  [1000, 'M'],
+  [500, 'D'],
+  [100, 'C'],
+  [50, 'L'],
+  [10, 'X'],
+  [5 'V'],
+  [1, 'I']
+]);
+let str=num.toString(),res=""
+	let i=0;
+	while(i<str.length-1){
+		if(str[i]<str[i+1]){
+			res+=map.get(str[i+1])
+			res+=map.get(str[i])
+			i+=2
+		}else{
+			res+=str[i]
+			i++
+		}
+	}
+	if(str[str.length-1]<str[str.length-2]){
+		res+=map.get(str[str.length-1])
+	}
+	console.log(res)
+	
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
