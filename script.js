bc9cf7bad01ab7b063b2d416d9ba03a468a69557
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-02 12:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,13 +12,20 @@ function convertToRoman(num) {
   //your code here
 	const map = new Map([
   [1000, 'M'],
+  [900, 'CM'],
   [500, 'D'],
+  [400, 'CD'],
   [100, 'C'],
+  [90, 'XC'],
   [50, 'L'],
+  [40, 'XL'],
   [10, 'X'],
-  [5 ,'V'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
   [1, 'I']
 ]);
+
 	let res=""
 for(let [value,symbol] of map){
 	while(value<=num){
